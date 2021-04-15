8cf7a09dc70e2cf5d04b4d1c93e6e07b55d60123
iannyip <ianyip@nullspace.com.sg>
2021-04-15 17:38:40 +0800

    on click adds new item to list

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Form = () => {
-  console.log("this is the form");
+const Form = ({ list, updateList }) => {
+  // State variables
+  const [field, setField] = useState("0");
+  // Callback function: for input
+  const changedField = (event) => {
+    const linkField = event.target.value;
+    setField(linkField);
+    console.log(field);
+  };
+  // Callback function: for button
+  const submitLink = (event) => {
+    const newList = [...list, field];
+    updateList(newList);
+  };
+  // return JSX
+  return (
+    <div>
+      <input value={field} onChange={changedField} />
+      <button type="submit" value="Click Me" onClick={submitLink}>
+        Click Me!
+      </button>
+    </div>
+  );
+};
+
+const LinkList = ({ links }) => {
+  console.log("this is the list");
+  return <div>Lnks</div>;
+};
+
+const ReadLinks = () => {
+  console.log("this is the list");
+  return <div>Read</div>;
 };
 
 export default function App() {
-  const [link, setLink] = useState(0);
-  return <div>This is App.jsx</div>;
+  const [links, setLinks] = useState([]);
+  console.log(`link list: ${links}`);
+  return (
+    <div>
+      This is App.jsx
+      <Form list={links} updateList={(linkArr) => setLinks(linkArr)} />
+      <LinkList list={links} />
+      <ReadLinks />
+    </div>
+  );
 }
